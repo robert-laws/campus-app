4824e14d045fa4e3c06e771ed528b282dafd0d15
Robert Laws <roblaws@me.com>
2022-04-27 16:50:11 -0500

    add button component

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import { Card } from './shared/Card';
+import { Button } from './shared/Button';
 
 export const ItemForm = () => {
   const [text, setText] = useState('');
+  const [rating, setRating] = useState(0);
 
   const handleTextChange = (e) => {
     setText(e.target.value);
+  };
+
+  const handleRatingChange = (e) => {
+    setRating(e.target.value);
   };
 
   return (
@@ -14,13 +20,22 @@ export const ItemForm = () => {
         <h2>How would you rate the item?</h2>
         {/* @todo - ratings select */}
         <div className='input-group'>
+          <select value={rating} onChange={handleRatingChange}>
+            {[...Array(11)].map((x, i) => (
+              <option value={i} key={i}>
+                {i}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className='input-group'>
           <input
             type='text'
             value={text}
             placeholder='Write a review'
             onChange={handleTextChange}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit'>Send</Button>
         </div>
       </form>
     </Card>
